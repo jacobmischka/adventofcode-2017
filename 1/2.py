c3c08c425b3d7aba76e0900af58c52a52c9871de
Jacob Mischka <jacob@mischka.me>
2017-12-01 10:42:13 -0600

    Complete day 1 using python

---
+++ 1/2.py
@@ -0,0 +1,15 @@
+from utils import get_input
+
+def main():
+	checksum = get_input('input.txt').strip()
+
+	s = 0
+	length = len(checksum)
+	for i, char in enumerate(checksum):
+		if char == checksum[(i + int(length / 2)) % length]:
+			s += int(char)
+
+	print(s)
+
+if __name__ == '__main__':
+	main()
