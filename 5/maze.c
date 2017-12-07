e3150a825d9594b9cf76dea4ed8130f19e266202
Jacob Mischka <jacob@mischka.me>
2017-12-06 21:04:05 -0600

    wip still

--- 5/maze.c
+++ 5/maze.c
@@ -7,16 +7,27 @@ typedef struct VecStruct {
 	int size;
 } Vec;
 
-int* increaseArray(int* arr, int newSize) {
-
+void printVec(Vec* vec, int toCapacity) {
+	printf("[\n");
+	int len = toCapacity
+		? vec->capacity
+		: vec->size;
+	for (int i = 0; i < len - 1; i++) {
+		printf("\t%d,\n", vec->arr[i]);
+	}
+	printf("\t%d\n", vec->arr[len - 1]);
+	printf("]\n");
 }
 
-void reallocateVec(Vec* vec, int newSize) {
+void reallocateVec(Vec** vec, int newSize) {
+	printVec(*vec, 1);
 	int* newArr = malloc(sizeof(int) * newSize);
-	for (int i = 0; i < vec->capacity; i++) {
-		newArr[i] = vec->arr[i];
+	for (int i = 0; i < (*vec)->capacity; i++) {
+		newArr[i] = (*vec)->arr[i];
 	}
-	vec->arr = newArr;
+	(*vec)->arr = newArr;
+	(*vec)->capacity = newSize;
+	printVec(*vec, 1);
 }
 
 Vec getInput() {
@@ -26,6 +37,7 @@ Vec getInput() {
 		.arr = malloc(sizeof(int) * initialSize),
 		.size = 0
 	};
+
 
 	FILE *fp;
 	char *line = NULL;
@@ -41,16 +53,12 @@ Vec getInput() {
 		instruction = atoi(line);
 		vec.arr[vec.size++] = instruction;
 		if (vec.size >= vec.capacity) {
-			reallocateVec(&vec, vec.capacity * 2);
+			reallocateVec(&&vec, vec.capacity * 2);
 		}
 	}
 
 	return vec;
 }
-
-// void printArr(int* arr) {
-// 	int len = sizeof(arr) / sizeof(int);
-// }
 
 void part1() {
 	int steps = 0;
