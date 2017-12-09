33c3a537001460afb89a9f3d7b1d0f87043a6c0e
Jacob Mischka <jacob@mischka.me>
2017-12-09 16:10:19 -0600

    Complete day 3 part 2 in scala

--- 3/Spiral.scala
+++ 3/Spiral.scala
@@ -1,3 +1,4 @@
+import scala.collection.mutable.HashMap
 import scala.io.Source
 import java.lang.Math
 
@@ -15,17 +16,124 @@ object Direction extends Enumeration {
 
 import Direction._
 
+class Grid {
+	val contents = new HashMap[Int, HashMap[Int, Int]]()
+	var radius = 0
+	var x = 0
+	var y = 0
+	var nextDirection = Right
+
+	set(0, 0, 1)
+
+	def set(x: Int, y: Int, value: Int): Unit = {
+		val col = contents.get(x) match {
+			case Some(col) => col
+			case None => {
+				new HashMap[Int, Int]()
+			}
+		}
+		col.update(y, value)
+		contents.update(x, col)
+	}
+
+	def get(x: Int, y: Int): Int = {
+		contents.get(x) match {
+			case Some(col) => {
+				col.get(y) match {
+					case Some(value) => value
+					case None => 0
+				}
+			}
+			case None => 0
+		}
+	}
+
+	def addRing(): Unit = {
+		radius += 1
+		nextDirection = Up
+	}
+
+	def posString(): String = {
+		"(" + x + ", " + y + ")"
+	}
+
+	def move(direction: Direction): Unit = {
+		direction match {
+			case Right => {
+				x += 1
+				if (x == radius + 1) {
+					addRing
+				}
+			}
+			case Up => {
+				y += 1
+				if (y == radius) {
+					nextDirection = Left
+				}
+			}
+			case Left => {
+				x -= 1
+				if (x == radius * -1) {
+					nextDirection = Down
+				}
+			}
+			case Down => {
+				y -= 1
+				if (y == radius * -1) {
+					nextDirection = Right
+				}
+			}
+		}
+	}
+
+	def adjacentSum(): Int = {
+		var sum = 0
+
+		sum += get(x, y + 1)
+		sum += get(x + 1, y + 1)
+		sum += get(x + 1, y)
+		sum += get(x + 1, y - 1)
+		sum += get(x, y - 1)
+		sum += get(x - 1, y - 1)
+		sum += get(x - 1, y)
+		sum += get(x - 1, y + 1)
+
+		sum
+	}
+
+	def add(): Int = {
+		move(nextDirection)
+		val value = adjacentSum
+		set(x, y, value)
+		value
+	}
+}
+
 object Spiral {
 	def part1(): Unit = {
 		print("Part 1: ")
 		println(getNumMoves(getInput.toInt))
 	}
 
+	def part2(): Unit = {
+		val target = getInput.toInt
+		val grid = new Grid()
+
+		var addedVal = 0
+		while (addedVal < target) {
+			addedVal = grid.add
+		}
+
+		print("Part 2: ")
+		println(addedVal)
+	}
+
 	def getNumMoves(target: Int): Int = {
 		val ringMoves = (getRingWidth(target) / 2.0).round
 		val ringSide = getRingSide(target)
 		val sideMoves = Math.abs(getSideCenter(getRingWidth(target), ringSide) - target).toInt
-		return (ringMoves + sideMoves - 1).toInt
+
+		(ringMoves + sideMoves - 1).toInt
 	}
 
 	def getSideCenter(ringWidth: Int, side: Direction): Int = {
@@ -40,13 +148,14 @@ object Spiral {
 	}
 
 	def getMoveToCenter(pos: Int): Direction = {
-		return Direction.opposite(getRingSide(pos))
+		Direction.opposite(getRingSide(pos))
 	}
 
 	def getSideNum(target: Int): Float = {
 		val ringWidth = getRingWidth(target)
 		val ringMax = Math.pow(ringWidth, 2).toInt
-		return (ringMax - target) / (ringWidth - 1).toFloat
+
+		(ringMax - target) / (ringWidth - 1).toFloat
 	}
 
 	def getRingSide(target: Int): Direction = {
@@ -57,7 +166,7 @@ object Spiral {
 			return Left
 		if (sideNum <= 3)
 			return Up
-		return Right
+		Right
 	}
 
 	def getRingWidth(target: Int): Int = {
@@ -65,14 +174,15 @@ object Spiral {
 		if (ring % 2 == 0)
 			ring += 1
 
-		return ring
+		ring
 	}
 
 	def getInput(): String = {
-		return Source.fromFile("./input/input.txt").mkString.trim
+		Source.fromFile("./input/input.txt").mkString.trim
 	}
 
 	def main(args: Array[String]): Unit = {
 		part1
+		part2
 	}
 }
